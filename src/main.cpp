8bab7e9a9d4c6fe09bdad38ce0d0cd42a63a40ae
CNawalol <awalolcn@gmail.com>
2023-11-11 14:50:31 +0800

    issue #1

--- src/main.cpp
+++ src/main.cpp
@@ -29,8 +29,8 @@ SPIClass V(HSPI);
 U8G2_SH1106_128X64_NONAME_F_4W_HW_SPI u8g2(U8G2_R0, OLED_SPI_CS, OLED_SPI_DC, OLED_SPI_RESET);
 Preferences preferences;
 Player player;
-UI* currentUI;
-Task button(2500,TASK_FOREVER,[](){
+UI *currentUI;
+Task button(2500, TASK_FOREVER, []() {
     currentUI->tick();
 });
 Scheduler runner;
@@ -46,7 +46,7 @@ void setup() {
     u8g2.begin();
     u8g2.enableUTF8Print();
     u8g2.setFont(u8g2_font_wqy12_t_gb2312);
-    u8g2.drawUTF8(20,20,"HelloWorld Setup");
+    u8g2.drawUTF8(20, 20, "HelloWorld Setup");
     u8g2.sendBuffer();
 
     // 初始化 SD
@@ -70,14 +70,14 @@ void setup() {
 
     player.scanSD();
 
-    for(const Song& s : player.playlist){
+    for (const Song &s: player.playlist) {
         Serial.println(s.name.c_str());
     }
 
     // 初始化按钮
-    pinMode(35,INPUT_PULLUP);
-    pinMode(34,INPUT_PULLUP);
-    pinMode(39,INPUT_PULLUP);
+    pinMode(35, INPUT_PULLUP);
+    pinMode(34, INPUT_PULLUP);
+    pinMode(39, INPUT_PULLUP);
 
     // 初始化定时器
     runner.init();
@@ -88,19 +88,20 @@ void setup() {
 
     currentUI = new LyricUI();
     currentUI->setup();
-}
 
-void debug(){
-    u8g2.clearBuffer();
-    u8g2.setFont(u8g2_font_wqy12_t_gb2312b);
-//    u8g2.drawUTF8(0,20,String(ESP.getFreeHeap()).c_str());
-    u8g2.drawUTF8(0,40,String("刮风这天我试着握着你手但偏偏雨渐渐大到我看你不见").c_str());
-    u8g2.sendBuffer();
+    xTaskCreate([](void *args) {
+        while (1) {
+            audio.loop();
+        }
+    }, "audio", 2048, NULL, 1, NULL);
+
+    xTaskCreate([](void *args) {
+        while (1) {
+            currentUI->loop();
+        }
+    }, "render", 2048, NULL, 2, NULL);
 }
 
 void loop() {
-    audio.loop();
     runner.execute();
-    currentUI->loop();
-//    debug();
 }
